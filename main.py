2dc7361990a2b5c5f11dce38b39c23303fa440c4
Lisa Wang <46847817+lisawym@users.noreply.github.com>
2023-10-31 22:09:56 +0000

    make the rust etl work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import psutil
+import os
+import time
 
 from mylib.extract import extract
 from mylib.transform_load import load_and_split
@@ -12,22 +14,73 @@ parser.add_argument(
     "--transform_load", action="store_true", help="Transform and load the data."
 )
 parser.add_argument("--query", action="store_true", help="Query the data.")
+parser.add_argument("--all", action="store_true", help="Run all ETL tasks.")
+parser.add_argument("--all_10", action="store_true", help="Run all ETL tasks 10 times.")
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 # Perform the requested ETL task
-if args.extract:
+if args.all:
+    start_time = time.time()
     process = psutil.Process()
     memory_info = process.memory_info()
-
-# Get the total memory used in MB
-    total_memory_used_mb = memory_info.rss / 1024**2
-
-# Print the total memory used
-    print(f"Total memory used: {total_memory_used_mb:.2f} MB")
     extract()
-elif args.transform_load:
     load_and_split()
-elif args.query:
     query("OfferDB.db")
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(f"Memory used: {memory_info.rss / 1024 / 1024:.2f} MB")
+    avg_cpu_usage = psutil.cpu_percent(interval=0.3)
+    print(f"Average CPU usage: {avg_cpu_usage}%")
+elif args.all_10:
+    total_time = 0
+    total_memory = 0
+    total_cpu_usage = 0
+    for i in range(10):
+        start_time = time.time()
+        process = psutil.Process()
+        memory_info = process.memory_info()
+        extract()
+        load_and_split()
+        query("OfferDB.db")
+        end_time = time.time()
+        total_time += end_time - start_time
+        total_memory += memory_info.rss / 1024 / 1024
+        total_cpu_usage += psutil.cpu_percent(interval=0.3)
+    print(f"Total time taken: {total_time:.2f} seconds")
+    print(f"Average time taken: {total_time / 10:.2f} seconds")
+    print(f"Total memory used: {total_memory:.2f} MB")
+    print(f"Average memory used: {total_memory / 10:.2f} MB")
+    print(f"Total average CPU usage: {total_cpu_usage:.2f}%")
+    print(f"Average CPU usage: {total_cpu_usage / 10:.2f}%")
+elif args.extract:
+    start_time = time.time()
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    extract()
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(f"Memory used: {memory_info.rss / 1024 / 1024:.2f} MB")
+    avg_cpu_usage = psutil.cpu_percent(interval=0.3)
+    print(f"Average CPU usage: {avg_cpu_usage}%")
+elif args.transform_load:
+    start_time = time.time()
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    load_and_split()
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(f"Memory used: {memory_info.rss / 1024 / 1024:.2f} MB")
+    avg_cpu_usage = psutil.cpu_percent(interval=0.3)
+    print(f"Average CPU usage: {avg_cpu_usage}%")
+elif args.query:
+    start_time = time.time()
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    query("OfferDB.db")
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(f"Memory used: {memory_info.rss / 1024 / 1024:.2f} MB")
+    avg_cpu_usage = psutil.cpu_percent(interval=0.3)
+    print(f"Average CPU usage: {avg_cpu_usage}%")
--- rust_etl/src/main.rs
+++ rust_etl/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+mod extract;
+mod transform_load;
+mod query;
+
+pub fn main() {
+    match std::env::args().nth(1).as_deref() {
+        Some("extract") => { extract::run().expect("Failed to run extract"); },
+        Some("transform_load") => { transform_load::main().expect("Failed to run transform_load"); },
+        Some("query") => { query::query().expect("Failed to run query"); },
+        _ => println!("Invalid command provided"),
+    };
 }
